3a2e499861ff87f012f5acb6f88fdf28bc380440
Riya <rs0735957@gmail.com>
2026-02-12 23:13:39 +0530

    Time: 0 ms (100%), Space: 43.3 MB (89.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,14 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n=nums.length;
-        int c=0;
-        int c1=0;
-        int c2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                c++;
-            }else if(nums[i]==1){
-                c1++;
-            }else{
-                c2++;
-            }
-        }
-        int idx=0;
-        for(int i=0;i<c;i++)
-        nums[idx++]=0;
-        for(int i=0;i<c1;i++)
-        nums[idx++]=1;
-        for(int i=0;i<c2;i++)
-        nums[idx++]=2;
+       int count0=0, count1=0, count2=0;
+       for(int num:nums){
+        if(num==0) count0++;
+        else if(num==1) count1++;
+        else count2++;
+       }
+       int idx=0;
+       while(count0-- >0) nums[idx++]=0;
+       while(count1-- >0) nums[idx++]=1;
+       while(count2-- >0) nums[idx++]=2;
     }
- 
 }
