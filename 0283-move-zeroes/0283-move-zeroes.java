4cab15822ad2666e09a220dd239899e1f2506f6c
Riya <rs0735957@gmail.com>
2026-06-05 20:31:56 +0530

    Time: 2 ms (91.65%), Space: 47.9 MB (36.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,39 +1,14 @@
-import java.util.*;
 class Solution {
-    static void moveZeroes(int[] nums) {
+    public void moveZeroes(int[] nums) {
         int n=nums.length;
-        int l=0;
-        for(int r=0;r<n;r++){
-            if(nums[r]!=0){
-                swap(nums,l,r);
-                l++;
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
             }
         }
-    }
-    static void swap(int[]nums,int x,int y){
-        int temp=nums[x];
-        nums[x]=nums[y];
-        nums[y]=temp;
-    }
-    static void printArr(int[]nums){
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            System.out.println(nums[i]);
-        }
-        System.out.println();
-    }
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter size of array:");
-        int n=sc.nextInt();
-        int[]nums=new int[n];
-        for(int i=0;i<n;i++){
-            nums[i]=sc.nextInt();
-        }
-        System.out.println("Original Array:");
-        printArr(nums);
-        moveZeroes(nums);
-        System.out.println("Sorted array:");
-        printArr(nums);
     }
 }
